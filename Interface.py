2726c775a4f2f541e491cd7d19da23fd1f70f558
Heloisa Prestes <heloisa.psg15@gmail.com>
2026-05-11 20:53:11 -0300

    teste(dashboard): Alinhamento dos KPIs

--- Interface.py
+++ Interface.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from sqlalchemy import create_engine, text
 import pandas as pd
+#import plotly.graph_objects as go
 
 app = Flask(__name__)
 
